1c296b2558403bb223bfcb38f92d06390c76c1a4
Ionică Bizău <bizauionica@gmail.com>
2017-04-26 12:58:27 +0300

    Support

--- example/index.js
+++ example/index.js
@@ -1,21 +1,27 @@
 const Daty = require("../lib");
 
 // You can initialize Daty using the date parameters
-var d = new Daty(2015, 11, 16);
+const d = new Daty(2015, 11, 16, 18, 42);
 
 // ...but you have friendly date formatting
 console.log(d.format("LL"));
 // => December 16, 2015
 
+console.log(d.format("LLLL"));
+// => Wednesday, December 16, 2015 6:42 PM
+
+console.log(d.utc().format("LLLL"));
+// => Wednesday, December 16, 2015 4:42 PM
+
 // Go back in the history, when the Romanian revolution started in 1989
 // We will clone the current date since we will need it later.
 // Also, subtract 26 years.
-var revolutionDate = d.clone().subtract(26, "years");
+const revolutionDate = d.clone().subtract(26, "years");
 console.log(revolutionDate.format("LLL"));
 // => December 16, 1989 12:00 AM
 
 // Make a difference in years
-var difference = new Daty().diff(revolutionDate, "years");
+const difference = new Daty().diff(revolutionDate, "years");
 
 console.log(`Right now there are ${difference} years since the Romanian revolution, 1989.`);
 // => Right now there are 26 years since the Romanian revolution, 1989.
